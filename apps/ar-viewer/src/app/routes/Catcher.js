d1d45edf47bf176fc10cbe43a95f164fdfecb728
devofficer <sompaqueen47@gmail.com>
2022-11-24 10:40:31 +0000

    user agent detection off to test oculus

--- apps/ar-viewer/src/app/routes/Catcher.js
+++ apps/ar-viewer/src/app/routes/Catcher.js
@@ -9,9 +9,11 @@ import TopBar from '../components/TopBar';
 import use8thWall from '../hooks/use8thWall';
 import Experience from '../components/Experience';
 import { AdaptiveDpr, Preload } from '@react-three/drei';
+import useStore from '../store';
 
 function Catcher() {
   const [canvasEl, setCanvasEl] = useState();
+
   const { XR8 } = use8thWall(process.env.NX_APP_8THWALL_API_KEY, canvasEl);
 
   const dimensions = {
