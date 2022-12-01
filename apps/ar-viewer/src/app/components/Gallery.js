a2dbc728ff004711f5b736826fb21658a10d036b
devofficer <sompaqueen47@gmail.com>
2022-12-01 02:29:17 +0000

    attempted optimizations

--- apps/ar-viewer/src/app/components/Gallery.js
+++ apps/ar-viewer/src/app/components/Gallery.js
@@ -6,15 +6,14 @@ import React, {
   useEffect,
   Fragment,
   useState,
+  Suspense,
 } from 'react';
 import * as THREE from 'three';
 import useStore from '../store';
 import { calculatePositions } from '../utils/calculatePositions';
-
 import { useFrame } from '@react-three/fiber';
-import randomcolor from 'randomcolor';
 import GalleryAsset from './GalleryAsset';
-import { useGLTF } from '@react-three/drei';
+import Ring from './Ring';
 
 const Gallery = ({ model }) => {
   const groupRef = useRef();
@@ -32,14 +31,14 @@ const Gallery = ({ model }) => {
     position: [0, 0, 0],
     scale: 1,
     rotation: [0, Math.PI, 0],
-    config: { friction: 40, duration: 500 },
+    config: { friction: 40 },
     immediate: false,
 
     onRest: () => {
       api.start((i) => {
         return {
-          position: [0, 0, 0],
           scale: 1,
+          opacity: 1,
         };
       });
     },
@@ -52,85 +51,42 @@ const Gallery = ({ model }) => {
     }, 500);
     api.start((i) => {
       return {
-        position: [0, -5, 0],
-        scale: 0.5,
+        scale: 0.7,
+        opacity: 0,
       };
     });
   }, [api, currentLevel, layerIndex]);
 
-  // useFrame((state, delta) => {
-  //   if (!isDesktopMode || itemDetails) return;
-  //   groupRef.current.rotation.y += delta * 0.075;
-  // });
+  useFrame((state, delta) => {
+    if (!isDesktopMode || itemDetails) return;
+    groupRef.current.rotation.y += delta * 0.075;
+  });
 
   const calculatedGalleryLayout = useMemo(() => {
     return activeQuasar.gallery.map((item, index) => {
-      return calculatePositions(item.assets.splice(0, 12), galleryRadius - 0.2);
+      return calculatePositions(item.assets, galleryRadius - 0.2);
     });
   }, [activeQuasar, galleryRadius]);
 
   const assetGallery = calculatedGalleryLayout[layerIndex];
 
-  const meshMaterial = {
-    side: THREE.BackSide,
-    transparent: false,
-    opacity: 1,
-    thickness: 3,
-    roughness: 0.6,
-    clearcoat: 0.9,
-    clearcoatRoughness: 0.3,
-    transmission: 1,
-    // ior: 1.2,
-    envMapIntensity: 5,
-    attenuationDistance: 5,
-  };
+  useEffect(() => {
+    model?.scene?.traverse((node) => {
+      if (node.isMesh) {
+        if (node.name.includes('QUASAR_SKIN_OUT_M_003')) {
+          domeRef.current.material = node.material;
+          domeRef.current.material.envMapIntensity = 1;
+        }
+      }
+    });
+  }, [model]);
 
-  const Ring = ({ offset, isRandom }) => {
-    return (
-      <mesh position={[0, offset, 0]}>
-        <cylinderGeometry
-          attach="geometry"
-          args={[galleryRadius, galleryRadius, 0.1, 32, 1, true]}
-        />
-        <meshPhysicalMaterial
-          attach="material"
-          {...meshMaterial}
-          color={isRandom ? randomcolor() : '#fff'}
-        />
-      </mesh>
-    );
-  };
-
-  // useEffect(() => {
-  //   model?.scene?.traverse((node) => {
-  //     if (node.isMesh) {
-  //       if (node.name.includes('QUASAR_SKIN_OUT_M_003')) {
-  //         domeRef.current.material = node.material;
-  //         domeRef.current.material.envMapIntensity = 1;
-  //       }
-  //     }
-  //   });
-  // }, [model]);
-
-  return (
-    <group ref={groupRef}>
-      {/* <mesh ref={domeRef}>
-        <sphereGeometry
-          attach="geometry"
-          args={[galleryRadius + 0.1, 32, 32, 0, Math.PI * 2, 0, 1.4]}
-        />
-        <meshStandardMaterial attach="material" side={THREE.DoubleSide} />
-      </mesh> */}
-
-      <animated.group {...spring}>
-        <Ring offset={-0.22} isRandom />
-        <Ring offset={-0.11} />
-        <Ring offset={0} isRandom />
-        <Ring offset={0.11} />
-        <Ring offset={0.22} isRandom />
-        {assetGallery?.map((asset, index) => {
-          return (
-            <Fragment key={index}>
+  const memoAssetGallery = useMemo(
+    () =>
+      assetGallery?.map((asset, index) => {
+        return (
+          <Fragment key={index}>
+            <Suspense fallback={null}>
               <GalleryAsset
                 initialPosition={[asset.x, asset.y, asset.z]}
                 initialRotation={[0, asset.rotation, 0]}
@@ -143,14 +99,32 @@ const Gallery = ({ model }) => {
                 description={asset?.description}
                 id={`asset-${index}-levelIndex-${currentLevel}`}
               />
-            </Fragment>
-          );
-        })}
-      </animated.group>
+            </Suspense>
+          </Fragment>
+        );
+      }),
+    [assetGallery, currentLevel],
+  );
+
+  return (
+    <group ref={groupRef}>
+      <mesh ref={domeRef}>
+        <sphereGeometry
+          attach="geometry"
+          args={[galleryRadius + 0.1, 32, 32, 0, Math.PI * 2, 0, 1.4]}
+        />
+        <meshStandardMaterial attach="material" side={THREE.DoubleSide} />
+      </mesh>
+
+      {/* Merge meshes and memoize these. https://github.com/pmndrs/drei#merged */}
+      <Ring offset={-0.22} isRandom radius={galleryRadius} />
+      <Ring offset={-0.11} radius={galleryRadius} />
+      <Ring offset={0} isRandom radius={galleryRadius} />
+      <Ring offset={0.11} radius={galleryRadius} />
+      <Ring offset={0.22} isRandom radius={galleryRadius} />
+      <animated.group {...spring}>{memoAssetGallery}</animated.group>
     </group>
   );
 };
-
-// https://codesandbox.io/s/jflps?file=/src/App.js:2700-2714
 
 export default memo(Gallery);
