2627ba6c628b1479cf3cd3ea5e94494e4ffe01f3
alexalampour <sompaqueen47@gmail.com>
2022-12-08 04:27:59 -0800

    fix: update Experience routing to use navigate of react-dom-router

--- apps/ar-viewer/src/app/components/Experience.js
+++ apps/ar-viewer/src/app/components/Experience.js
@@ -1,61 +1,58 @@
-import { memo, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useThree } from '@react-three/fiber';
-import useStore from '../store';
+import { disposeAll } from 'utils/disposeAll';
+import useStore from 'store';
 import Core from './Core';
-import { disposeAll } from '../utils/disposeAll';
 import axios from 'axios';
-import { useSpring, animated } from '@react-spring/three';
 
 const Experience = ({ XR8 }) => {
-  const { renderer, scene, camera } = XR8.Threejs.xrScene();
-  const setDefaultCamera = useThree(({ set }) => set);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const navigate = useNavigate();
   const npointId = useStore((state) => state.npointId);
   const activeQuasar = useStore((state) => state.activeQuasar);
   const setProjectData = useStore((state) => state.setProjectData);
   const selectedQuasar = useStore((state) => state.selectedQuasar);
+  const setThreeState = useThree(({ set }) => set);
+  const { renderer, scene, camera } = XR8.Threejs.xrScene();
 
   useEffect(() => {
     if (activeQuasar) return;
-    console.log('Load remote data');
-    setIsLoaded(false);
     // not sure if this is fully clearing the scene
     disposeAll(scene);
     renderer.renderLists.dispose();
     axios
       .get(`https://api.npoint.io/${npointId}`)
-      .then((response) => {
-        setProjectData(response.data, selectedQuasar);
-        // set react router path params
-        // history.push(`/experience/${selectedQuasar}`);
-        var newurl =
-          window.location.protocol +
-          '//' +
-          window.location.host +
-          window.location.pathname +
-          `?projectId=${npointId}&quasarId=${selectedQuasar}`;
-        window.history.pushState({ path: newurl }, '', newurl);
+      .then((res) => {
+        setProjectData(res.data, selectedQuasar);
+        navigate(`/catcher/?projectId=${npointId}&quasarId=${selectedQuasar}`);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [npointId, selectedQuasar, setProjectData, activeQuasar, scene, renderer]);
+  }, [
+    npointId,
+    selectedQuasar,
+    setProjectData,
+    activeQuasar,
+    scene,
+    renderer,
+    navigate,
+  ]);
 
   const appRef = useRef();
 
   useEffect(() => {
     if (scene && activeQuasar && camera) {
-      console.log('Init Scene');
       camera.position.y = 1.8;
-      // Set the default camera to use ThreeJS's camera
-      setDefaultCamera({
+      // Set 8th wall camera and scene as three.js instances
+      setThreeState({
         camera,
         scene,
       });
       // Add the app to 8thWall's ThreeJS scene
       scene.add(appRef.current);
     }
-  }, [scene, camera, activeQuasar, setDefaultCamera]);
+  }, [scene, camera, activeQuasar, setThreeState]);
 
   return <group ref={appRef}>{activeQuasar ? <Core /> : null}</group>;
 };
